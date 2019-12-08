13256b7915fdcbe4c6b74bbd7609285452900902
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2019-12-08 01:20:36 -0700

    Fix more stuff

--- build-deb.sh
+++ build-deb.sh
@@ -29,8 +29,8 @@ echo 'Version: '$(sed -nr 's/^.*"version": "(.*)",.*$/\1/p' ./usr/share/webthing
 echo 'Priority: optional' >> ./DEBIAN/control
 echo 'Architecture: '$(dpkg --print-architecture) >> ./DEBIAN/control
 echo 'Maintainer: gucci-on-fleek' >> ./DEBIAN/control
-echo 'Description: webthings-gateway' >> ./DEBIAN/control
-echo 'Depends: python2, nodejs (='$(apt-cache show nodejs | awk '/Version/ {print $2}')'), npm (>=3.5)' >> ./DEBIAN/control
+echo 'Description: Mozilla WebThings Gateway' >> ./DEBIAN/control
+echo 'Depends: python2.7, nodejs (='$(apt-cache show nodejs | awk '/Version/ {print $2}')'), npm (>=3.5)' >> ./DEBIAN/control
 
 cd ..
 dpkg-deb --build webthings-gateway
